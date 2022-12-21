a1d6cc1f08896cd0cc8b7d41f19c818caf6499da
Akash <na.akash715@gmail.com>
2022-12-21 18:53:09 +0600

    polished

--- src/Pages/Dashboard/Dashboard/Dashboard.js
+++ src/Pages/Dashboard/Dashboard/Dashboard.js
@@ -1,16 +1,23 @@
 import React, { useContext } from 'react';
 import { AuthContext } from '../../../contexts/AuthProvider';
 import useTitle from '../../../hooks/useTitle';
+import img from '../../../assets/icons/12.png';
 
 const Dashboard = () => {
     useTitle('Dashboard');
     const { user } = useContext(AuthContext);
     return (
         <div className='bg-slate-100 mx-auto' style={{ height: '100vw' }}>
-            <div className='py-40 my-auto  pt-64'>
-                <h2 className='text-3xl mb-16 '>Hey, <span className='text-primary font-semibold'>{user?.displayName}</span></h2>
-                <h2 className='text-xl mb-4 font-semibold'>Welcome To</h2>
-                <h1 className='text-3xl text-secondary font-medium'>Wheel Mania <br /> Dashboard</h1>
+
+
+            <div className=' my-auto  pt-56'>
+
+                <h2 className='text-3xl mb-10 '>Hey, <span className='text-primary font-semibold uppercase'>{user?.displayName}</span></h2>
+                <h2 className='text-xl mb-4 font-semibold ml-7'>Welcome To</h2>
+                <div className='text-center my-8 '>
+                    <img src={img} alt="" className='w-1/4 mx-auto' />
+                </div>
+                <h1 className='text-3xl text-secondary font-medium uppercase ml-7'>Dashboard</h1>
             </div>
         </div>
     );
